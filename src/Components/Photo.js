7e1e7fcae138b4a98106ecfbc6c79c0b2698dffc
Christina <christina@Postmans-MacBook-Pro.local>
2019-07-01 13:44:29 -0700

    added more styling to phot grid and photo component

--- src/Components/Photo.js
+++ src/Components/Photo.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 
 class Photo extends Component {
+    // this refers to the component instance that passes in the props
     render() {
         const post = this.props.post
-        return <figure></figure>
+        return <figure> </figure>
     }
 }
 
--- src/Components/PhotoWall.js
+++ src/Components/PhotoWall.js
@@ -5,8 +5,9 @@ class PhotoWall extends Component {
     render() {
         // access each post as prop
         // map through posts and for each instance we generate photo instance from each post
-        return <div>
-            {this.props.posts.map((post, index) => <Photo key={index} prop={post}/>)} 
+        // pass className as an attribute to add css styles
+        return <div className="photoGrid">
+            {this.props.posts.map((post, index) => <Photo key={index} post={post} />)} 
         </div>
     }
 }
