f6300e26b82c216f220b7f1dea58e5eb974ba3dc
Christina <christina@Postmans-MacBook-Pro.local>
2019-07-01 11:47:14 -0700

    refractor content to render  as components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDom from 'react-dom'
 
 // when using an array, every element should be represented by a unique key
 const tasks = ['Take out the trash', 'Buy Milk','Clean house'];
 
-// react elements decide what is rendered
-const element = React.createElement('ol', null, tasks
-.map((task, index) => React.createElement('li', { key: index}, task))); // tag, props (unique identifier), content
+// 1. react elements decide what is rendered
+// const element = React.createElement('ol', null, tasks
+// .map((task, index) => React.createElement('li', { key: index}, task))); // tag, props (unique identifier), content
 
 
 
 
-// react Dom does the rendering
-ReactDom.render(element, document.getElementById('root'));
+class List extends Component {
+    render() {
+        return (
+            <ol>{ tasks.map((task) => <li key={task}>{task}</li>)}</ol>
+        )
+    }
+}
+
+class Title extends Component {
+    render() {
+        return (
+            <h1> Task list </h1>
+        )
+    }
+}
+
+class Main extends Component {
+    render() {
+        return <div> 
+                <Title />
+                <List />
+            </div>
+    }
+}
+
+// 2. react Dom does the rendering
+ReactDom.render(<Main />, document.getElementById('root'));
