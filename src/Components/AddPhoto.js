63d2ad73a6124877d7a0db8b6651b4cd0390ecf4
Christina <christina.hastenrath@getpostman.com>
2019-07-03 15:21:54 -0700

    setup UI for add a photo page

--- src/Components/AddPhoto.js
+++ src/Components/AddPhoto.js
@@ -1,8 +1,22 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+
+
+// import { createBrowserHistory } from 'history'
 
 class AddPhoto extends Component {
     render() {
-        return <h1> This is adding photos</h1>
+        return (
+            <div>
+                <h1> Add a Photo </h1>
+                <div>
+                    <form className="form"> 
+                        <input type="text" placeholder="Link"/>
+                        <input type="text" placeholder="Description"/>
+                        <button className="">Post</button>
+                    </form>
+                </div>
+            </div>
+        )
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
+
+// import { createBrowserHistory } from 'history'
 
 import Main from './Components/Main'
 
